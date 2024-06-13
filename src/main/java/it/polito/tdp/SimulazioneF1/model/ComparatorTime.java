d86121f92c28ae75aa2cab6db020d5f7a0cb7551
TheLostUchica <126084333+TheLostUchica@users.noreply.github.com>
2024-06-13 21:13:20 +0200

    Update and rename CamparatorTime.java to ComparatorTime.java

--- src/main/java/it/polito/tdp/SimulazioneF1/model/ComparatorTime.java
+++ src/main/java/it/polito/tdp/SimulazioneF1/model/ComparatorTime.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class CamparatorTime implements Comparator<Map.Entry<Pilota, Double>>{
+public class ComparatorTime implements Comparator<Map.Entry<Pilota, Double>>{
 
 	@Override
 	public int compare(Entry<Pilota, Double> o1, Entry<Pilota, Double> o2) {
